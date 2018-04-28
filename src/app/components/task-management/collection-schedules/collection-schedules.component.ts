39e4a6b50fd8dc15c98a3ede2bb58c72dc9b9d69
Shanaka <madushanka991@gmail.com>
2018-04-28 21:34:06 +0530

    Added component for Create collection schedules

---
+++ src/app/components/task-management/collection-schedules/collection-schedules.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-collection-schedules',
+  templateUrl: './collection-schedules.component.html',
+  styleUrls: ['./collection-schedules.component.scss']
+})
+export class CollectionSchedulesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/components/task-management/task-management.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CollectionSchedulesComponent } from './collection-schedules/collection-schedules.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [CollectionSchedulesComponent]
+})
+export class TaskManagementModule { }
