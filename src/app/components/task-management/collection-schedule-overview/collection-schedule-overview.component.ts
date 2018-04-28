5c87f8b5f89e33c91f0b6a3c5e98ca41bac411ef
Shanaka <madushanka991@gmail.com>
2018-04-28 23:32:49 +0530

    Schedule overview component added

---
+++ src/app/components/task-management/collection-schedule-overview/collection-schedule-overview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-collection-schedule-overview',
+  templateUrl: './collection-schedule-overview.component.html',
+  styleUrls: ['./collection-schedule-overview.component.scss']
+})
+export class CollectionScheduleOverviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
