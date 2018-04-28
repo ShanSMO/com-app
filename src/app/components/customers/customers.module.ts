9e3ac9c9defece64ca7d2f833ce5928721eef2ad
Shanaka <madushanka991@gmail.com>
2018-04-28 23:47:46 +0530

    Customer module added

---
+++ src/app/components/customers/customers.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CustomerOverviewComponent } from './customer-overview/customer-overview.component';
+import { CustomerCreateComponent } from './customer-create/customer-create.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [CustomerOverviewComponent, CustomerCreateComponent]
+})
+export class CustomersModule { }
