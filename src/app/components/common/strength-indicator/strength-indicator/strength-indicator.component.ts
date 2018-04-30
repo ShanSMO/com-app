27b67cfe0c04f433a64fc269f4cb31208fa3fd1f
Shanaka <madushanka991@gmail.com>
2018-04-30 11:40:52 +0530

    sign up component modified

--- src/app/components/common/strength-indicator/strength-indicator/strength-indicator.component.ts
+++ src/app/components/common/strength-indicator/strength-indicator/strength-indicator.component.ts
@@ -11,10 +11,17 @@ export class StrengthIndicatorComponent implements OnInit, OnChanges {
 
   progressSize: any = 0;
   progressColor: any;
+  alertBackground: any;
   increment: any = 20;
 
   @Input() type: any;
   @Input() inputText: any;
+
+  hasLowerCase = false;
+  hasUpperCase = false;
+  hasNumber = false;
+  hasSpecialChar = false;
+  hasGoodLength = false;
 
   constructor() { }
 
@@ -32,22 +39,37 @@ export class StrengthIndicatorComponent implements OnInit, OnChanges {
 
       if (new RegExp(Regex.REGEX_DIGITS).test(text)) {
         this.progressSize = this.progressSize + this.increment;
+        this.hasNumber = true;
+      } else {
+        this.hasNumber = false;
       }
 
       if (new RegExp(Regex.REGEX_LOWER_CASE).test(text)) {
         this.progressSize = this.progressSize + this.increment;
+        this.hasLowerCase = true;
+      } else {
+        this.hasLowerCase = false;
       }
 
       if (new RegExp(Regex.REGEX_SPECIAL_CHARACTER).test(text)) {
         this.progressSize = this.progressSize + this.increment;
+        this.hasSpecialChar = true;
+      } else {
+        this.hasSpecialChar = false;
       }
 
       if (new RegExp(Regex.REGEX_UPPER_CASE).test(text)) {
         this.progressSize = this.progressSize + this.increment;
+        this.hasUpperCase = true;
+      } else {
+        this.hasUpperCase = false;
       }
 
       if (text.length > 8) {
         this.progressSize = this.progressSize + this.increment;
+        this.hasGoodLength = true;
+      } else {
+        this.hasGoodLength = false;
       }
     }
 
@@ -57,6 +79,7 @@ export class StrengthIndicatorComponent implements OnInit, OnChanges {
   setColor(strength) {
     if (strength === 20) {
       this.progressColor =  '#ad1010';
+      this.alertBackground = 'rgba(173,16,16,0.3)'
     }
 
     if (strength === 40) {
