e820aca6fbdf7af307ab97906e50584e51008207
Shanaka <madushanka991@gmail.com>
2018-05-06 11:49:19 +0530

    added accounting component

---
+++ src/app/components/accounting/accounting.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DailySummaryComponent } from './daily-summary/daily-summary.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [DailySummaryComponent]
+})
+export class AccountingModule { }
